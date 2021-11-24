c6d25982f0a8236b397a40476f3a59834215e47f
Erlend Dahl <erlend.dahl@sintef.no>
2021-11-24 15:33:26 +0100

    Reintroduced slightly modified frame processor logic in PcapBrowser.

--- pcapBrowser.py
+++ pcapBrowser.py
@@ -3,13 +3,16 @@ from open3dVisualizer import Open3DVisualizer
 
 class PcapBrowser:
 
-    def __init__(self, pcapPath, metaDataPath):
+    def __init__(self, pcap_path, metadata_path):
         """Initialize a PcapBrowser by reading metadata and setting
         up a package source from the pcap file.
         """
 
-        self.reader = BufferedPcapReader(pcapPath, metaDataPath)
+        self.reader = BufferedPcapReader(pcap_path, metadata_path)
         self.vis = Open3DVisualizer()
+
+        self.cloud_processors = [None]
+        self.cloud_processor_index = 0
     
     def start_visualization(self):
         """Initializes an open3d visualizer, configures it to use arrow
@@ -29,8 +32,13 @@ class PcapBrowser:
             if not self.set_frame(self._currentFrame):
                 self._currentFrame += 1
 
+        def key_toggle_thinning(vis):
+            self.cloud_processor_index = (self.cloud_processor_index + 1) % len(self.cloud_processors)
+            self.setFrame(self._currentFrame)
+
         self.vis.register_key_callback(262, key_next) # Arrow right
         self.vis.register_key_callback(263, key_prev) # Arrow left
+        self.vis.register_key_callback(80, key_toggle_thinning) # P
         # List of key codes can be found here: https://www.glfw.org/docs/latest/group__keys.html
 
         self.set_frame(0)
@@ -44,7 +52,8 @@ class PcapBrowser:
         the geometry object containing the current frame. If the current frame is
         empty (end of file), this function does nothing, and returns False."""
 
-        frame = self.reader.read_frame(num)
+        frame = self.read_frame(num)
+
         if frame is None:
             return False
         
@@ -55,6 +64,17 @@ class PcapBrowser:
         print("Showing frame ", num)
 
         return True
+
+    def read_frame(self, num:int):
+        """Retrieves the current frame from the reader object, processes it (if activated), and converts it to an open3d geometry."""
+
+        frame = self.reader.read_frame(num)
+
+        # If a cloud processor is active, process the cloud (for example by voxel thinning)
+        if self.cloud_processors[self.cloud_processor_index] is not None:
+            frame = self.cloud_processors[self.cloud_processor_index].process(frame)
+
+        return frame
         
 
 if __name__ == "__main__":
