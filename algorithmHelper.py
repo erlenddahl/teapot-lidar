22181b627d2bfa01e613121112ff94221280c377
Erlend Dahl <erlend.dahl@sintef.no>
2023-04-12 10:50:27 +0200

    Commented failing algorithms.

--- algorithmHelper.py
+++ algorithmHelper.py
@@ -2,7 +2,7 @@ from matchers.nicp import NicpMatcher
 from matchers.downsamplefirst import DownsampleFirstNicpMatcher
 from matchers.globalregistrationfirst import GlobalFirstNicpMatcher
 from matchers.fastglobalregistrationfirst import FastGlobalFirstNicpMatcher
-from matchers.probregmatchers import CpdMatcher, FilterregMatcher
+#from matchers.probregmatchers import CpdMatcher, FilterregMatcher
 
 class AlgorithmHelper:
 
@@ -20,11 +20,11 @@ class AlgorithmHelper:
         AlgorithmHelper._add_algorithm(DownsampleFirstNicpMatcher(0.05), "Downsample (0.05), then NICP")
         AlgorithmHelper._add_algorithm(GlobalFirstNicpMatcher(), "Global registration, then NICP")
         AlgorithmHelper._add_algorithm(FastGlobalFirstNicpMatcher(), "Fast global registration, then NICP")
-        AlgorithmHelper._add_algorithm(CpdMatcher(tf_type_name="rigid"), "CPD rigid")
-        AlgorithmHelper._add_algorithm(CpdMatcher(tf_type_name="affine"), "CPD affine")
-        AlgorithmHelper._add_algorithm(CpdMatcher(tf_type_name="nonrigid"), "CPD nonrigid")
-        AlgorithmHelper._add_algorithm(FilterregMatcher(), "Filterreg pt2pt")
-        AlgorithmHelper._add_algorithm(FilterregMatcher(objective_type="pt2pl"), "Filterreg pt2pl")
+        #AlgorithmHelper._add_algorithm(CpdMatcher(tf_type_name="rigid"), "CPD rigid")
+        #AlgorithmHelper._add_algorithm(CpdMatcher(tf_type_name="affine"), "CPD affine")
+        #AlgorithmHelper._add_algorithm(CpdMatcher(tf_type_name="nonrigid"), "CPD nonrigid")
+        #AlgorithmHelper._add_algorithm(FilterregMatcher(), "Filterreg pt2pt")
+        #AlgorithmHelper._add_algorithm(FilterregMatcher(objective_type="pt2pl"), "Filterreg pt2pl")
 
         return AlgorithmHelper.algorithms
 
