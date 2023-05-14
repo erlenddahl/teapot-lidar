6eb48a36d238f5b7114e8dfdc259f7eaf4c657d4
Erlend Dahl <erlend.dahl@sintef.no>
2023-05-14 22:48:58 +0200

    Fixed missing arguments.

--- recreateCaches.py
+++ recreateCaches.py
@@ -5,17 +5,19 @@ import os
 from glob import glob
 
 from pcap.pcapReader import PcapReader
+from pcap.pcapReaderHelper import PcapReaderHelper
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--root-directory', type=str, required=True, help="Recreate caches for all pcap files found recursively under the given root directory.")
-    parser.add_argument('--sbet', type=str, required=False, help="The path to a corresponding SBET file with GNSS coordinates.")
     parser.add_argument('--recreate-caches', action='store_true', required=False, help="If True, existing caches will be re-created. If False, files with existing caches will be skipped.")
+    PcapReaderHelper.add_sbet_arguments(parser)
     args = parser.parse_args()
 
     # The PCAP reader expects this argument, but it's not relevant here, so we always set it to False.
     args.skip_last_frame_in_pcap_file = False
+    args.max_frame_radius = 25
 
     files = list([x for x in chain.from_iterable(glob(os.path.join(x[0], '*.pcap')) for x in os.walk(args.root_directory)) if not x.endswith(".pcap.meta.json")])
 
